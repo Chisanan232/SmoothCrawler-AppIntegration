c0f2e2f754dea3b941c826437621cee109178b52
Chisanan232 <chi10211201@cycu.org.tw>
2022-06-16 21:37:54 +0800

    [New Feature] (code) Add framework module to build architecture and rule object.

---
+++ appintegration/task/framework.py
@@ -0,0 +1,32 @@
+from typing import Any, Optional, Iterable
+from abc import ABCMeta, ABC, abstractmethod
+
+
+
+class ApplicationIntegrationTask(metaclass=ABCMeta):
+
+    @abstractmethod
+    def init(self, *args, **kwargs) -> Any:
+        pass
+
+
+    @abstractmethod
+    def close(self) -> None:
+        pass
+
+
+
+class ApplicationIntegrationSourceTask(ApplicationIntegrationTask, ABC):
+
+    @abstractmethod
+    def generate(self, data: Iterable[Iterable]) -> Optional[Any]:
+        pass
+
+
+
+class ApplicationIntegrationProcessorTask(ApplicationIntegrationTask, ABC):
+
+    @abstractmethod
+    def acquire(self) -> Optional[Any]:
+        pass
+
