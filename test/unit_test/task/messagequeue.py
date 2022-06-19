0590ab1f08aee3c9daf46bdab4f92a519963f19f
Chisanan232 <chi10211201@cycu.org.tw>
2022-06-20 07:18:48 +0800

    [Breaking Change] (test) Add testing description for each items at spec 'MessageQueueTaskTestSpec'. [skip ci]

--- test/unit_test/task/messagequeue.py
+++ test/unit_test/task/messagequeue.py
@@ -1,7 +1,7 @@
 from appintegration.task.framework import ApplicationIntegrationSourceTask, ApplicationIntegrationProcessorTask
 from appintegration.task.messagequeue import (
     MessageQueueConfig, MessageQueueTask,
-    KafkaConfig, KafkaTask,
+    KafkaConfig, KafkaTask
 )
 
 from typing import Any, Union, TypeVar, Generic, Iterable
@@ -57,34 +57,91 @@ class MessageQueueConfigTestSpec(metaclass=ABCMeta):
 
 class MessageQueueTaskTestSpec:
 
+    """
+    Spec of testing items about objects **XXXTask** of module *appintegration.task.messagequeue*.
+    In other words, it means that for the sub-class of object **MessageQueueTask**.
+    """
+
     @pytest.fixture(scope="class")
     def config(self) -> Generic[_MQTask]:
+        """
+        Which object **MessageQueueConfig** it should test for. This is the default of
+        functions *config_for_producer* and *config_for_consumer*. So it could only
+        override this function, rather than 2 others to let both of them return the same
+        instance **MessageQueueConfig**.
+
+        :return: The instance of sub-class of **MessageQueueConfig** and it's the default
+                      of other 2 functions *config_for_producer* and *config_for_consumer*.
+        """
+
         pass
 
 
     @pytest.fixture(scope="class")
     def config_for_producer(self, config) -> Generic[_MQTask]:
+        """
+        The object is sub-class of **MessageQueueConfig** to test. This object for
+        component *Processor Application* or be called *Producer*.
+
+        :param config: The default value. Please see more detail in function *config*.
+        :return: The instance of object for component *Processor Application* or be called *Producer*.
+        """
+
         pass
 
 
     @pytest.fixture(scope="class")
     def config_for_consumer(self, config) -> Generic[_MQTask]:
+        """
+        The object is sub-class of **MessageQueueConfig** to test. This object for
+        component *Source Application* or be called *Consumer*.
+
+        :param config: The default value. Please see more detail in function *config*.
+        :return: The instance of object for component *Source Application* or be called *Consumer*.
+        """
+
         pass
 
 
     @pytest.fixture(scope="class")
     @abstractmethod
     def task_for_generating(self, config_for_producer: Generic[_MsgQueueConfig]) -> Generic[_MQTask]:
+        """
+        The object for test which is responsible of generating something in source application.
+        In *Producer* speaking, it means publish something (in generally, it's a message) to
+        message middle component system.
+
+        :param config_for_producer: The instance of sub-class of **MessageQueueConfig** for *Source* (or means *Producer*).
+        :return: The instance of sub-class of **MessageQueueTask**.
+        """
+
         pass
 
 
     @pytest.fixture(scope="class")
     @abstractmethod
     def task_for_acquiring(self, config_for_consumer: Generic[_MsgQueueConfig]) -> Generic[_MQTask]:
+        """
+        The object for test which is responsible of acquire something in processor application.
+        In *Consumer* speaking, it means subscribe to get something (in generally, it's a message)
+        from message middle component system.
+
+        :param config_for_consumer: The instance of sub-class of **MessageQueueConfig** for *Processor* (or means *Consumer*).
+        :return: The instance of sub-class of **MessageQueueTask**.
+        """
+
         pass
 
 
     def test_publish_and_subscribe_features(self, task_for_generating: KafkaTask, task_for_acquiring: KafkaTask):
+        """
+        This is the major function to run testing. It would test producer and consumer in the same time.
+
+        :param task_for_generating: The instance of sub-class of **MessageQueueTask** for *Producer*.
+        :param task_for_acquiring: The instance of sub-class of **MessageQueueTask** for *Consumer*.
+        :return: None
+        """
+
         _test_consumer = threading.Thread(target=self._subscribing_process, args=(task_for_acquiring, ))
         _test_producer = threading.Thread(target=self._publishing_process, args=(task_for_generating, ))
 
@@ -101,12 +158,12 @@ class MessageQueueTaskTestSpec:
 
     def _publishing_process(self, _task: KafkaTask) -> None:
         """
-        The truly implementation of generating feature usage.
+        The truly implementation of producing feature usage.
         The running procedure is:
 
-            Run the implementation -> Check the running result -> Run some processes finally
+            Run the producing implementation -> Check the running result -> Run some processes finally
 
-        * Run the implementation: test_generate
+        * Run the implementation: _sending_feature
         * Check the running result: _chk_generate_running_result
         * Run some processes finally: _chk_generate_final_ps
 
@@ -133,17 +190,26 @@ class MessageQueueTaskTestSpec:
 
     @abstractmethod
     def _sending_feature(self, _task: KafkaTask, topic: str, value: bytes) -> None:
+        """
+        The truly implement of sending (or it calls publishing) something to message middle component system.
+
+        :param _task: The instance of sub-class of **MessageQueueTask** for *Producer*.
+        :param topic: The target topic where the message would be send to.
+        :param value: The message it would be send to system.
+        :return: None
+        """
+
         pass
 
 
     def _subscribing_process(self, _task: KafkaTask) -> None:
         """
-        The truly implementation of acquiring feature usage.
+        The truly implementation of consuming feature usage.
         The running procedure is:
 
-            Run the implementation -> Check the running result -> Run some processes finally
+            Run the consuming implementation -> Check the running result -> Run some processes finally
 
-        * Run the implementation: test_acquire
+        * Run the implementation: _poll_feature
         * Check the running result: _chk_acquire_running_result
         * Run some processes finally: _chk_acquire_final_ps
 
@@ -163,27 +229,60 @@ class MessageQueueTaskTestSpec:
 
     @abstractmethod
     def _poll_feature(self, _task: KafkaTask) -> None:
+        """
+        The truly implement of polling (or it calls consuming) something from message middle component system.
+
+        :param _task: The instance of sub-class of **MessageQueueTask** for *Consumer*.
+        :return: None
+        """
+
         pass
 
 
     @property
     @abstractmethod
     def _testing_topic(self) -> str:
+        """
+        Which the topic it would send (or publish) message to.
+
+        :return: A string type value which is topic name.
+        """
+
         pass
 
 
     @property
     def _testing_data(self) -> Union[Iterable[Iterable], Any]:
+        """
+        The data for testing which would be sent (or published) to message middle component system.
+
+        :return: In generally, it would be a iterable object.
+        """
+
         return [bytes(f"This is testing {i} message.", "utf-8") for i in range(TestingMessageCnt)]
 
 
     @abstractmethod
     def _chk_generate_running_result(self, **kwargs) -> None:
+        """
+        The checking of the producing running result.
+
+        :param kwargs: Some arguments if it needs.
+        :return: None
+        """
+
         pass
 
 
     @abstractmethod
     def _chk_acquire_running_result(self, **kwargs) -> None:
+        """
+        The checking of the consuming running result.
+
+        :param kwargs: Some arguments if it needs.
+        :return: None
+        """
+
         pass
 
 
