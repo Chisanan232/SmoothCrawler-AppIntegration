f694f599f29221f2030c95463b52f4590a6a1b1e
Chisanan232 <chi10211201@cycu.org.tw>
2022-06-25 11:39:43 +0800

    [New Feature] (test) Add testing data a about a URL and a list of URLs.

--- test/_data.py
+++ test/_data.py
@@ -1,6 +1,10 @@
 from typing import List, Dict
 
 
+Test_URL = "http://www.example.com"
+
+Test_URLs_List = ["http://www.example.com", "http://www.example.com", "http://www.example.com"]
+
 Test_Data_List: List = [
     ["http://www.example.com", "GET"],
     ["http://www.example.com", "POST"],
