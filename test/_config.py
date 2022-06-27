a0fb9753f99dc2773cf9e5080bf84b41e6d5fd51
Chisanan232 <chi10211201@cycu.org.tw>
2022-06-28 07:05:49 +0800

    [New Feature] (test) Add some settings for HTTP.

--- test/_config.py
+++ test/_config.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 
+# For file based settings
 Test_CSV_File_Path: str = str(Path("./for_testing.csv"))
 Test_XLSX_File_Path: str = str(Path("./for_testing.xlsx"))
 Test_JSON_File_Path: str = str(Path("./for_testing.json"))
@@ -11,3 +12,22 @@ Test_Writing_Mode: str = "a+"
 Test_XML_Writing_Mode: str = "wb"
 Test_Reading_Mode: str = "r"
 
+
+# For file based settings
+
+class DummyObj:
+
+    def __str__(self):
+        return "DummyObj"
+
+    def __repr__(self):
+        return self.__str__()
+
+    @property
+    def attr(self) -> str:
+        return "testing_attribute"
+
+
+Test_HTTP_Parameters = {"date": "20220625", "code": "6666"}
+Test_HTTP_Body = DummyObj()
+Test_HTTP_Content_Type = "application/json"
