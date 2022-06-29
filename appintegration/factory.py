5296282e67afae8b77098d207842b47fbdc1f70d
Chisanan232 <chi10211201@cycu.org.tw>
2022-06-29 18:19:51 +0800

    [New Feature + Breaking Change] (code) Modify the object ApplicationIntegrationFactory.
    1. Add new attributes *app_source_role* and *data_handling_before_back*.
    
    2. Rename the attribute *app_processor_role* from *app_integration_role*.

--- appintegration/factory.py
+++ appintegration/factory.py
@@ -1,22 +1,49 @@
 from smoothcrawler.factory import _chk_factory_type, CrawlerFactory as _CrawlerFactory
-from typing import Generic
+from typing import TypeVar, Generic
 
 from .role.framework import ApplicationIntegrationRole
+from .components import DataHandlerBeforeBack
 from .types import BaseRole
 
+
+_DataHandlerBeforeBack = TypeVar("_DataHandlerBeforeBack", bound=DataHandlerBeforeBack)
 
 
 class ApplicationIntegrationFactory(_CrawlerFactory):
 
-    _ApplicationIntegrationRole: BaseRole = None
+    _AppIntegrationSourceRole: BaseRole = None
+    _DataHandlerBeforeBackFactory: _DataHandlerBeforeBack = None
+    _AppIntegrationProcessorRole: BaseRole = None
 
     @property
-    def app_integration_role(self) -> Generic[BaseRole]:
-        return self._ApplicationIntegrationRole
+    def app_source_role(self) -> Generic[BaseRole]:
+        return self._AppIntegrationSourceRole
 
 
-    @app_integration_role.setter
-    def app_integration_role(self, role: Generic[BaseRole]) -> None:
+    @app_source_role.setter
+    def app_source_role(self, role: Generic[BaseRole]) -> None:
         _chk_factory_type(role, ApplicationIntegrationRole)
-        self._ApplicationIntegrationRole = role
+        self._AppIntegrationSourceRole = role
+
+
+    @property
+    def data_handling_before_back(self) -> Generic[_DataHandlerBeforeBack]:
+        return self._DataHandlerBeforeBackFactory
+
+
+    @data_handling_before_back.setter
+    def data_handling_before_back(self, role: Generic[_DataHandlerBeforeBack]) -> None:
+        _chk_factory_type(role, DataHandlerBeforeBack)
+        self._DataHandlerBeforeBackFactory = role
+
+
+    @property
+    def app_processor_role(self) -> Generic[BaseRole]:
+        return self._AppIntegrationProcessorRole
+
+
+    @app_processor_role.setter
+    def app_processor_role(self, role: Generic[BaseRole]) -> None:
+        _chk_factory_type(role, ApplicationIntegrationRole)
+        self._AppIntegrationProcessorRole = role
 
