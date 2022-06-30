50e3dd619318e338b3e283877a18d60a3d19e55d
Chisanan232 <chi10211201@cycu.org.tw>
2022-06-30 16:56:39 +0800

    [New Feature] (test) Add testing spec for module 'crawler'.

---
+++ test/integration_test/crawler/_spec.py
@@ -0,0 +1,241 @@
+from appintegration.role.framework import ApplicationIntegrationRole
+from appintegration.task.framework import ApplicationIntegrationTask
+from appintegration.crawler import AppIntegrationCrawler
+from appintegration.factory import ApplicationIntegrationFactory
+
+from ._components import RequestsHTTPRequest, RequestsStockHTTPResponseParser, ExampleWebDataHandler, DataFilePersistenceLayer, StockDataHandlerBeforeBack
+
+from typing import Iterable, TypeVar, Generic
+from abc import ABCMeta, abstractmethod
+import pytest
+
+
+_ApplicationIntegrationRole = TypeVar("_ApplicationIntegrationRole", bound=ApplicationIntegrationRole)
+_ApplicationIntegrationTask = TypeVar("_ApplicationIntegrationTask", bound=ApplicationIntegrationTask)
+_AppIntegrationCrawler = TypeVar("_AppIntegrationCrawler", bound=AppIntegrationCrawler)
+_ApplicationIntegrationFactory = TypeVar("_ApplicationIntegrationFactory", bound=ApplicationIntegrationFactory)
+
+
+class CrawlerTestSpec(metaclass=ABCMeta):
+    """
+    Test spec for testing object which is sub-class of **AppIntegrationCrawler** in module *crawler*.
+    """
+
+    @pytest.fixture(scope="class")
+    def factory(self) -> _ApplicationIntegrationFactory:
+        """
+        Initial object **ApplicationIntegrationFactory** and set the instances.
+
+        :return: An instance of object **ApplicationIntegrationFactory** which saves
+                      some instances to let *crawler* to use in running process.
+        """
+
+        _factory = ApplicationIntegrationFactory()
+        _factory.http_factory = RequestsHTTPRequest()
+        _factory.parser_factory = RequestsStockHTTPResponseParser()
+        _factory.data_handling_factory = ExampleWebDataHandler()
+        _factory.persistence_factory = DataFilePersistenceLayer()
+        _factory.app_processor_role = self.processor_role()
+        _factory.app_source_role = self.source_role()
+        _factory.data_handling_before_back = StockDataHandlerBeforeBack()
+        return _factory
+
+
+    @abstractmethod
+    def processor_role(self) -> Generic[_ApplicationIntegrationRole]:
+        """
+        Which type of *application processor role* to use in crawler testing.
+
+        :return: An instance of object **ApplicationIntegrationRole**.
+        """
+
+        pass
+
+
+    @abstractmethod
+    def task(self, **kwargs) -> Generic[_ApplicationIntegrationTask]:
+        """
+        Which task it would be used by *role* (*application source* or *application processor*)
+        to run testing.
+
+        :param kwargs: Some parameters for different type object **ApplicationIntegrationTask**.
+        :return: An instance of object **ApplicationIntegrationTask**
+        """
+
+        pass
+
+
+    @abstractmethod
+    def source_role(self) -> Generic[_ApplicationIntegrationRole]:
+        """
+        Which type of *application source role* to use in testing or for crawler's method *run_and_back_to_middle*.
+
+        :return: An instance of object **ApplicationIntegrationRole**.
+        """
+
+        pass
+
+
+    @pytest.fixture(scope="class")
+    @abstractmethod
+    def crawler(self, factory: _ApplicationIntegrationFactory) -> Generic[_AppIntegrationCrawler]:
+        """
+        Which *crawler* should be test and it would be initial with **ApplicationIntegrationFactory**.
+
+        :param factory: An instance of object **ApplicationIntegrationFactory** which has saved some factories.
+        :return: An instance of object **AppIntegrationCrawler**.
+        """
+
+        pass
+
+
+    def test_get_targets(self, crawler: _AppIntegrationCrawler) -> None:
+        """
+        Test for getting targets for crawler to run with it.
+
+        :param crawler: The instance of object **AppIntegrationCrawler**.
+        :return: None
+        """
+
+        self._prepare_target_data()
+
+        try:
+            _targets = crawler.get_target()
+
+            self._chk_result_of_getting_targets(_targets=_targets)
+        finally:
+            self._final_process()
+
+
+    @abstractmethod
+    def _chk_result_of_getting_targets(self, _targets: Iterable) -> None:
+        """
+        Checking the running result of getting targets.
+
+        :param _targets: An iterable object which saves info like URLs, HTTP method, etc about targets.
+        :return: None
+        """
+
+        pass
+
+
+    def test_run(self, crawler: _AppIntegrationCrawler) -> None:
+        """
+        Test for running crawler generally and get the result.
+
+        :param crawler: The instance of object **AppIntegrationCrawler**.
+        :return: None
+        """
+
+        self._prepare_target_data()
+
+        try:
+            _targets = crawler.get_target()
+            _results = []
+            for _target in _targets:
+                _result = crawler.run(target=_target)
+                _results.append(_result)
+
+            self._chk_result_of_running(_results=_results)
+        finally:
+            self._final_process()
+
+
+    @abstractmethod
+    def _chk_result_of_running(self, _results: Iterable) -> None:
+        """
+        Checking the running result of running crawler.
+
+        :param _results: The results data of running crawler.
+        :return: None
+        """
+
+        pass
+
+
+    def test_run_and_save(self, crawler: _AppIntegrationCrawler) -> None:
+        """
+        Test for running crawler and save the result data.
+
+        :param crawler: The instance of object **AppIntegrationCrawler**.
+        :return:
+        """
+
+        self._prepare_target_data()
+
+        try:
+            _targets = crawler.get_target()
+            for _target in _targets:
+                crawler.run_and_save(target=_target)
+
+            _factory = crawler.factory
+            self._chk_result_of_running_and_saving(_factory=_factory)
+        finally:
+            self._final_process()
+
+
+    @abstractmethod
+    def _chk_result_of_running_and_saving(self, _factory) -> None:
+        """
+        Checking the running result of crawler running and saving.
+
+        :param _factory: The instance of using factory **ApplicationIntegrationFactory**.
+        :return: None
+        """
+
+        pass
+
+
+    def test_run_and_back_to_middle(self, crawler: _AppIntegrationCrawler) -> None:
+        """
+        Test for running crawler and let the result data back to application integration middle system.
+
+        :param crawler: The instance of object **AppIntegrationCrawler**.
+        :return:
+        """
+
+        self._prepare_target_data()
+
+        try:
+            _targets = crawler.get_target()
+            for _target in _targets:
+                crawler.run_and_back_to_middle(target=_target)
+
+            _factory = crawler.factory
+            self._chk_result_of_running_and_backing_to_middle()
+        finally:
+            self._final_process()
+
+
+    @abstractmethod
+    def _chk_result_of_running_and_backing_to_middle(self) -> None:
+        """
+        Check the running result of crawler running and let result data back to application integration middle system.
+
+        :return: None
+        """
+
+        pass
+
+
+    @abstractmethod
+    def _prepare_target_data(self) -> None:
+        """
+        Prepare the targets before run the testing of crawler.
+
+        :return: None
+        """
+
+        pass
+
+
+    @abstractmethod
+    def _final_process(self) -> None:
+        """
+        Do something finally in testing process.
+
+        :return: None
+        """
+
+        pass
+
