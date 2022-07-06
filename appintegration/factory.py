26e9c37505344e7c0a6311809d90b4ef40d664a8
Chisanan232 <chi10211201@cycu.org.tw>
2022-07-06 21:52:07 +0800

    [Breaking Change] (code) Modify the importing.

--- appintegration/factory.py
+++ appintegration/factory.py
@@ -1,28 +1,28 @@
 from smoothcrawler.factory import _chk_factory_type, CrawlerFactory as _CrawlerFactory
 from typing import TypeVar, Generic
 
-from .role.framework import ApplicationIntegrationRole
-from .components import DataHandlerBeforeBack
-from .types import BaseRole
+from .role.framework import ApplicationIntegrationRole as _ApplicationIntegrationRole
+from .components import DataHandlerBeforeBack as _DataHandlerBeforeBackObj
 
 
-_DataHandlerBeforeBack = TypeVar("_DataHandlerBeforeBack", bound=DataHandlerBeforeBack)
+_BaseRole = TypeVar("_BaseRole", bound=_ApplicationIntegrationRole)
+_DataHandlerBeforeBack = TypeVar("_DataHandlerBeforeBack", bound=_DataHandlerBeforeBackObj)
 
 
 class ApplicationIntegrationFactory(_CrawlerFactory):
 
-    _AppIntegrationSourceRole: BaseRole = None
+    _AppIntegrationSourceRole: _BaseRole = None
     _DataHandlerBeforeBackFactory: _DataHandlerBeforeBack = None
-    _AppIntegrationProcessorRole: BaseRole = None
+    _AppIntegrationProcessorRole: _BaseRole = None
 
     @property
-    def app_source_role(self) -> Generic[BaseRole]:
+    def app_source_role(self) -> Generic[_BaseRole]:
         return self._AppIntegrationSourceRole
 
 
     @app_source_role.setter
-    def app_source_role(self, role: Generic[BaseRole]) -> None:
-        _chk_factory_type(role, ApplicationIntegrationRole)
+    def app_source_role(self, role: Generic[_BaseRole]) -> None:
+        _chk_factory_type(role, _ApplicationIntegrationRole)
         self._AppIntegrationSourceRole = role
 
 
@@ -33,17 +33,17 @@ class ApplicationIntegrationFactory(_CrawlerFactory):
 
     @data_handling_before_back.setter
     def data_handling_before_back(self, role: Generic[_DataHandlerBeforeBack]) -> None:
-        _chk_factory_type(role, DataHandlerBeforeBack)
+        _chk_factory_type(role, _DataHandlerBeforeBackObj)
         self._DataHandlerBeforeBackFactory = role
 
 
     @property
-    def app_processor_role(self) -> Generic[BaseRole]:
+    def app_processor_role(self) -> Generic[_BaseRole]:
         return self._AppIntegrationProcessorRole
 
 
     @app_processor_role.setter
-    def app_processor_role(self, role: Generic[BaseRole]) -> None:
-        _chk_factory_type(role, ApplicationIntegrationRole)
+    def app_processor_role(self, role: Generic[_BaseRole]) -> None:
+        _chk_factory_type(role, _ApplicationIntegrationRole)
         self._AppIntegrationProcessorRole = role
 
