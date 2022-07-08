495f1b43567ef6a89f661a3ac63af9092b7c7cde
Chisanan232 <chi10211201@cycu.org.tw>
2022-07-08 14:36:01 +0800

    [Breaking Change] (code) Add checking mechanism about importing optional dependencies in module __init__.

--- appintegration/task/__init__.py
+++ appintegration/task/__init__.py
@@ -1,4 +1,11 @@
+from .. import _has_kafka_pkg, _has_pika_pkg, _has_stomp_pkg
 from .filebased import CSVTask, XLSXTask, JSONTask, XMLTask, PropertiesTask
 from .shareddatabase import SharedDatabaseTask
 from .directconnect import PipeTask, SocketTask
-from .messagequeue import KafkaTask, RabbitMQTask, ActiveMQTask
+
+if _has_kafka_pkg():
+    from .messagequeue import KafkaTask
+if _has_pika_pkg():
+    from .messagequeue import RabbitMQTask
+if _has_stomp_pkg():
+    from .messagequeue import ActiveMQTask
