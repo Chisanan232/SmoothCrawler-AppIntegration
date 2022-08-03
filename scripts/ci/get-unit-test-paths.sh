3d91df0418f8bb9260f06f0d5394a61cfbee3513
Chisanan232 <chi10211201@cycu.org.tw>
2022-08-03 09:17:10 +0800

    [Breaking Change + Style] (script) Fix some shell script issues
    1. Let variables be assigned correct data type.
    
    2. Fix the issue about 'Double quote to prevent globbing and word splitting'.
    
    3. Fix the issue about script cannot get all testing item paths correctly.

--- scripts/ci/get-unit-test-paths.sh
+++ scripts/ci/get-unit-test-paths.sh
@@ -4,12 +4,12 @@ set -ex
 runtime_os=$1
 
 declare -a init_tests
-declare -a utils_file_tests
 declare -a task_tests
 declare -a role_tests
+declare -a utils_file_tests
 
 getalltests() {
-    declare -a testpatharray=( "$(ls -F $1 | grep -v '/$' | grep -v '__init__.py' | grep -v 'test_config.py' | grep -v -E '^_[a-z_]{1,64}.py' | grep -v '__pycache__')")
+    declare -a testpatharray=( $(ls -F "$1" | grep -v '/$' | grep -v '__init__.py' | grep -v 'test_config.py' | grep -v -E '^_[a-z_]{1,64}.py' | grep -v '__pycache__'))
 
     declare -a alltestpaths
     for (( i = 0; i < ${#testpatharray[@]}; i++ )) ; do
@@ -18,15 +18,15 @@ getalltests() {
 
     if echo "$1" | grep -q "task";
     then
-        task_tests=${alltestpaths[@]}
+        task_tests=("${alltestpaths[@]}")
     elif echo "$1" | grep -q "role";
     then
-        role_tests=${alltestpaths[@]}
+        role_tests=("${alltestpaths[@]}")
     elif echo "$1" | grep -q "utils" | grep -q "file";
     then
-        utils_file_tests=${alltestpaths[@]}
+        utils_file_tests=("${alltestpaths[@]}")
     else
-        init_tests=${alltestpaths[@]}
+        init_tests=("${alltestpaths[@]}")
     fi
 }
 
