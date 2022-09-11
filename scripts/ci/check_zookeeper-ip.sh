2aaea7f1762fd2f37a97c4f26537296288578555
Chisanan232 <chi10211201@cycu.org.tw>
2022-09-12 07:02:48 +0800

    [Breaking Change] (script) Add exit code.

--- scripts/ci/check_zookeeper-ip.sh
+++ scripts/ci/check_zookeeper-ip.sh
@@ -9,6 +9,8 @@ fi
 zookeeper_ip=$(/usr/bin/docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$zookeeper_docker_container_name")
 if [ "$zookeeper_ip" == "172.18.0.2" ]; then
     echo "✅📇 It's correct and expected IP address of Zookeeper service."
+    exit 0
 else
     echo "❌ It's incorrect and unexpected IP address of Zookeeper service, please let DevOps engineer to check it."
+    exit 1
 fi
