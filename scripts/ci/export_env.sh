9e59bbdc983d62310400b76d19e593f933690599
Chisanan232 <chi10211201@cycu.org.tw>
2022-09-11 17:30:26 +0800

    [Bug Fix] (script) Fix the issue about incorrect file path.

--- scripts/ci/export_env.sh
+++ scripts/ci/export_env.sh
@@ -7,13 +7,15 @@ set -ex
 ##   . ./export-env.sh ; echo ${MINIENTREGA_FECHALIMITE}
 
 runtime_os=$1
+env_file_path=$2
+
 if echo "$runtime_os" | grep -q 'Linux'; then
 
-  export $(grep -v '^#' .env | xargs -d '\n')
+  export $(grep -v '^#' "$env_file_path" | xargs -d '\n')
 
 elif echo "$runtime_os" | grep -q 'macOS'; then
 
-  export $(grep -v '^#' .env | xargs -0)
+  export $(grep -v '^#' "$env_file_path" | xargs -0)
 
 else
   echo "The $runtime_os OS is invalid."
