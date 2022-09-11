af5fea923feebe11244e65ac05718fbd01967599
Chisanan232 <chi10211201@cycu.org.tw>
2022-09-11 22:36:07 +0800

    [New Feature] (script) Add new shell script to check the IP address of Zookeeper is expected.

---
+++ scripts/ci/check_zookeeper-ip.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#set -ex
+
+zookeeper_docker_container_name=$1
+if [ "$zookeeper_docker_container_name" == "" ]; then
+    zookeeper_docker_container_name="zookeeper"
+fi
+zookeeper_ip=$(/usr/bin/docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$zookeeper_docker_container_name")
+if "$zookeeper_ip" == "172.18.0.2"; then
+    echo "✅📇 It's correct and expected IP address of Zookeeper service."
+else
+    echo "❌ It's incorrect and unexpected IP address of Zookeeper service, please let DevOps engineer to check it."
+fi
