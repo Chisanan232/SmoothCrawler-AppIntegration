a87df5703f15726fb5aa0352b5c65fc26a2fcc9e
Chisanan232 <chi10211201@cycu.org.tw>
2022-09-12 07:01:45 +0800

    [Bug Fix] (script) Fix issue about incorrect if-condition shell syntax.

--- scripts/ci/check_zookeeper-ip.sh
+++ scripts/ci/check_zookeeper-ip.sh
@@ -7,7 +7,7 @@ if [ "$zookeeper_docker_container_name" == "" ]; then
     zookeeper_docker_container_name="zookeeper"
 fi
 zookeeper_ip=$(/usr/bin/docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$zookeeper_docker_container_name")
-if "$zookeeper_ip" == "172.18.0.2"; then
+if [ "$zookeeper_ip" == "172.18.0.2" ]; then
     echo "✅📇 It's correct and expected IP address of Zookeeper service."
 else
     echo "❌ It's incorrect and unexpected IP address of Zookeeper service, please let DevOps engineer to check it."
